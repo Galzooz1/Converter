077fcf543af9da1fd60c215d17b681197dac9aec
Gal <galzooz1@gmail.com>
2021-04-06 00:23:59 +0300

    55

--- src/comps/exchangeFooter.js
+++ src/comps/exchangeFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ExchangeFooter(props){
     return(
         <div className="bg-dark text-info text-center p-3">
-            Created By Gal Aluf &copy;
+            &copy; {new Date().getFullYear()} Created By Gal Aluf
         </div>
     )
 }
