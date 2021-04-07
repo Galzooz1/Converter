5957fcd13f93252ae405e1d39c4f73c46eb63a98
Gal <galzooz1@gmail.com>
2021-04-07 14:10:54 +0300

    555

--- src/comps/exchangeBody.js
+++ src/comps/exchangeBody.js
@@ -15,7 +15,7 @@ function ExchangeBody(props){
         let clock = time.getHours() + ':' + (time.getMinutes()) + ':' + time.getSeconds();
     
     const doApi = async() => {
-        let url = `http://apilayer.net/api/live?access_key=3c81786f9b3d2e267f40d08af97b97f2&currencies=usd,ils,eur,btc,thb`;
+        let url = `json/money.json`;
         try{
             let resp = await fetch(url)
             let data = await resp.json();
