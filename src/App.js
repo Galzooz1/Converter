3130a105348a26f567de1866e435d3a1d1fad0e8
Gal <galzooz1@gmail.com>
2021-04-11 13:48:47 +0300

    555

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import ExchangeHeader from './comps/exchangeHeader';
 import ExchangeBody from './comps/exchangeBody';
 import ExchangeFooter from './comps/exchangeFooter';
 
 function App() {
   return (
     <div className="App">
-      <ExchangeHeader />
       <ExchangeBody/>
       <ExchangeFooter/>
     </div>
--- src/comps/exchangeBody.js
+++ src/comps/exchangeBody.js
@@ -31,37 +31,11 @@ function ExchangeBody(props){
         }
     }
         return(
+        <React.Fragment>
+        <header className="bg-dark">
+           <h1 className="text-info display-6">Wave Currency Converter App </h1>
+        </header>
         <main>
-            {/* <div className="box col-lg-6 mx-auto">
-                <h2 className="text-primary">Current Rate by USD</h2>
-                <div className="rates">
-                    <div className="change">
-                    <h3>USD</h3>
-                    <h3>{exAr.USDUSD}</h3>
-                    <h3></h3>
-                    </div>
-                    <div className="change">
-                    <h3>Euro</h3>
-                    <h3>{exAr.USDEUR}</h3>
-                    <h3></h3>
-                    </div>
-                    <div className="change">
-                    <h3>BITCOIN</h3>
-                    <h3>{exAr.USDBTC}</h3>
-                    <h3></h3>
-                    </div>
-                    <div className="change">
-                    <h3>THB</h3>
-                    <h3>{exAr.USDTHB}</h3>
-                    <h3></h3>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                    <h3>ILS</h3>
-                    <h3>{exAr.USDILS}</h3>
-                    <h3></h3>
-                    </div>
-                </div>
-            </div> */}
             <div className="bg-white rounded-3 col-lg-10 mx-auto border border-dark m-5">
                 <ExchangeInput exAr={exAr}/>
             </div>
@@ -70,6 +44,7 @@ function ExchangeBody(props){
                 <h3>{date.toLocaleTimeString()}</h3>
             </div>
         </main>
+            </React.Fragment>
     )
 }
 
