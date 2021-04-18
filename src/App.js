4c87e80352b691abf1f35631338c580844e873d9
Gal <galzooz1@gmail.com>
2021-04-18 14:27:31 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import MapsHeader from './project/mapsHeader';
 import MapsBody from './project/mapsBody';
+import { BrowserRouter as Route,Switch, Router } from 'react-router-dom';
+import MapsNav from './project/mapsNav';
 
 function App() {
   return (
     <div className="App">
-      <MapsHeader/>
+      <Router>
+      {/* <MapsHeader/> */}
+      <MapsNav/>
+      <Route exact path={"/"} component={MapsBody} />
+      <Route exact path={"/country/"} component={MapsBody} />
+      <Route exact path={"/country/:name"} component={MapsBody} />
+      <Route exact path={"/country/:countryCode"} component={MapsBody} />
+      </Router>
       <MapsBody/>
     </div>
   );
---
+++ src/project/mapsNav.js
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react';
+// import { useHistory } from 'react-router';
+// import { Link } from 'react-router-dom';
+import { Link,useHistory } from "react-router-dom"
+
+
+function MapsNav(props){
+    let searchRef = useRef();
+    let history = useHistory();
+    const searchCountry = () => { 
+        if(searchRef.current.value == ""){
+            alert("Please enter Country name!")
+        }else{
+            history.push("/country/"+ searchRef.current.value);
+            searchRef.current.value = "";
+        }
+    }
+    return(
+        <header className="container-fluid bg-dark">
+       <nav className="container text-white d-flex align-items-center justify-content-between">
+            <div className="row">
+                <Link to={"/country/USA"} className="text-decoration-none">USA</Link>
+                <Link to={"/country/israel"} className="text-decoration-none">ISRAEL</Link>
+                <Link to={"/country/UK"} className="text-decoration-none">UK</Link>
+                <Link to={"/country/France"} className="text-decoration-none">FRANCE</Link>
+                <Link to={"/country/Thailand"} className="text-decoration-none">THAILAND</Link>
+                <Link to={"/country/Japan"} className="text-decoration-none">JAPAN</Link>
+                <Link to={"/country/Brazil"} className="text-decoration-none">BRAZIL</Link>
+                <Link to={"/country/Ethiopia"} className="text-decoration-none">ETHIOPA</Link>
+                <Link to={"/country/China"} className="text-decoration-none">CHINA</Link>
+            </div>
+            <div className="p-2 d-flex align-items-center">
+                <label className="form-label me-2">Search</label>
+                <input ref={searchRef} type="text" className="input shadow"/>
+            </div>
+                <button onClick={searchCountry} className="btn btn-primary ms-1">🔍</button>
+        </nav>
+        </header>
+    )
+}
+
+export default MapsNav
